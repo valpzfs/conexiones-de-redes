d551af5dd88a6d0ed661689416a63e5bd580eea1
Amparo <amparoalcarazt@gmail.com>
2025-11-21 16:55:38 -0600

    Update input

--- ActInt2.cpp
+++ ActInt2.cpp
@@ -10,6 +10,10 @@
 #include <algorithm>
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
+#define Edge pair<int,int> // a donde llego y su costo
+#define Graph vector<vector<Edge>>
+#define MAX 400
 using namespace std;
 
 struct Colonia{
@@ -25,11 +29,34 @@ struct Colonia{
 	}
 };
 
+void initMatAdj(int matAdj [MAX][MAX]){
+    for(int i =0; i<MAX;i++){
+        matAdj [i][i]=0;
+        for(int j = i+1; j<MAX;j++){
+            matAdj[i][j] = matAdj[j][i]= INT_MAX;
+        }
+    }
+}
+void leeDatos(int matAdj[MAX][MAX], Graph &G, int edges,unordered_map<string, int> index){
+    string col1, col2;
+    int c1,c2;
+    int cost;
+    for(int i=0;i<edges; i++){
+        cin>>col1>>col2>>cost;
+        int c1 = index[col1];
+        int c2 = index[col2];
+        matAdj[c1][c2]= matAdj[c2][c1]=cost;
+        Edge edge1(c2,cost);
+        G[c1].push_back(edge1);
+        Edge edge2(c1,cost);
+        G[c2].push_back(edge2);
+    }
+}
 int main(){
-
     int n,m ,q, k; //n = cantidad de colonias, m = número de conexiones entre colonias, k = las conexiones con el nuevo cableado, q = futuras nuevas colonias que se desean conectar.
     cin >> n >> m >> q >> k;
     vector<Colonia> colonias(n);
+    unordered_map<string, int> index;
     for(int i=0; i<n;i++){
         string nom;
         int x, y;
@@ -39,15 +66,11 @@ int main(){
         colonias[i].x=x;
         colonias[i].y=y;
         colonias[i].central=c;
+        index[nom]=i;
     }
-    // for(int i=0; i<m;i++){
-        
-
-
-    // }
-
-
-
-
+    Graph G(m);
+    int matAdj[MAX][MAX];
+    initMatAdj(matAdj);
+    leeDatos(matAdj, G, m, index);
     return 0;
 }
