94e38ce4ce9c1fdba105b6623a8a9b9f90ecb41d
valpzfs <valerialopezf04@gmail.com>
2025-11-21 16:09:02 -0600

    main cpp ya bien

---
+++ ActInt2.cpp
@@ -0,0 +1,26 @@
+// E2. Actividad Integradora 2
+// 21 de noviembre del 2025
+/*
+    EQUIPO:
+    Amparo Alcaraz Tonella A01253885
+    Valeria López Flores A00838648
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+
+
+int main(){
+
+    int n,m ,q, k;
+    cin >> n >> m >> q >> k;
+
+
+
+
+    return 0;
+}
