cad2b13b6b8f97447b5340f4f3d0451e45e8f913
valpzfs <valerialopezf04@gmail.com>
2025-11-22 13:39:50 -0600

    Update ActInt2.cpp

--- ActInt2.cpp
+++ ActInt2.cpp
@@ -9,7 +9,6 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
-#include <sstream>
 #include <unordered_map>
 #define Edge pair<int,int> // a donde llego y su costo
 #define Graph vector<vector<Edge>>
@@ -21,6 +20,15 @@ struct Colonia{
     int x;
     int y; 
     bool central;
+
+    // faltaba poner un constructor por defecto ya que el de abajo es ya con parametros :D
+    Colonia(){
+        nombre = "";
+        x=0;
+        y=0;
+        central = false;
+    }
+
     Colonia(string nombre, int x, int y, bool central){
 		this->nombre = nombre;
 		this->x = x;
@@ -52,9 +60,10 @@ void leeDatos(int matAdj[MAX][MAX], Graph &G, int edges,unordered_map<string, in
         G[c2].push_back(edge2);
     }
 }
+
 int main(){
-    int n,m ,q, k; //n = cantidad de colonias, m = número de conexiones entre colonias, k = las conexiones con el nuevo cableado, q = futuras nuevas colonias que se desean conectar.
-    cin >> n >> m >> q >> k;
+    int n,m,k,q; //n = cantidad de colonias, m = número de conexiones entre colonias, k = las conexiones con el nuevo cableado, q = futuras nuevas colonias que se desean conectar.
+    cin >> n >> m >> k >> q;
     vector<Colonia> colonias(n);
     unordered_map<string, int> index;
     for(int i=0; i<n;i++){
@@ -68,9 +77,27 @@ int main(){
         colonias[i].central=c;
         index[nom]=i;
     }
-    Graph G(m);
+
+    Graph G(n);
     int matAdj[MAX][MAX];
     initMatAdj(matAdj);
     leeDatos(matAdj, G, m, index);
+
+    vector<pair<int,int>> cables(k);
+    for(int i=0;i<k;i++){
+        string col1,col2;
+        cin>> col1>>col2;
+        cables[i] = {index[col1], index[col2]};
+    }
+
+    vector<Colonia> newColonias(q);
+    for(int i=0; i<q; i++){
+        string nombre;
+        int x,y;
+        cin >> nombre >> x>> y;
+        newColonias.push_back(Colonia(nombre,x,y,false)); // no habia necesidad de declarar una variable para el bool de central ya que no es algo que nos sea util jajaja
+    }
+
+    
     return 0;
 }
